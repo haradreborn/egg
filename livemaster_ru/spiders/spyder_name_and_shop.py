ab92e32b8ea96fa1b8c9473172d7bcf44ae641cc
haradreborn <alexey.shabanov@deliveryhero.com>
2015-06-30 00:13:56 +0200

    rebase

--- livemaster_ru/spiders/spyder_name_and_shop.py
+++ livemaster_ru/spiders/spyder_name_and_shop.py
@@ -2,23 +2,17 @@ from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 from livemaster_ru.items import LivemasterRuItem
 from scrapy.http import Request
-from scrapy.contrib.spiders import CrawlSpider, Rule
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 
 
 class MySpider(BaseSpider):
     name = "name_and_shop"
-    allowed_domains = ["livemaster.ru"]
-    start_urls = [
-        "http://www.livemaster.ru/catalogue/kartiny-i-panno"]
-
-    rules = Rule(SgmlLinkExtractor(allow=()),
-        follow=True, callback='parse')
+    allowed_domains = ["http://www.livemaster.ru/catalogue/kartiny-i-panno"]
+    start_urls = ["http://www.livemaster.ru/catalogue/kartiny-i-panno"]
 
     def parse(self, response):
         hxs = HtmlXPathSelector(response)
         names = hxs.select('//div/div/div[@class="author"]/a').extract()
         for name in names:
             item = LivemasterRuItem()
-            item["name"] = name
+            item["name"] = name[9:]
             yield item
