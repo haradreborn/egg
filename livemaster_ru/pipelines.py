8ade68fcf2371606ae9011a8b7a96c4c2291a407
haradreborn <alexey.shabanov@deliveryhero.com>
2015-06-30 23:41:59 +0200

    release livemaster_ru

--- livemaster_ru/pipelines.py
+++ livemaster_ru/pipelines.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
-
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy.exceptions import DropItem
 
 
 class LivemasterRuPipeline(object):
+
+    def __init__(self):
+        self.pages_seen = set()
+
     def process_item(self, item, spider):
-        return item
+        if item['page'] in self.pages_seen:
+            raise DropItem("Duplicate item found: %s" % item)
+        else:
+            self.pages_seen.add(item['page'])
+            return item
